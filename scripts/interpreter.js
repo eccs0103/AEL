17e32feb7bb46dcc48f0966dd2c8d06aafba1e83
eccs0103 <eccs0103@gmail.com>
2023-01-24 22:11:03 +0400

    0.2.5

--- scripts/interpreter.js
+++ scripts/interpreter.js
@@ -3,17 +3,13 @@ try {
 	const formInterpreter = (/** @type {HTMLFormElement} */ (document.querySelector(`form#interpreter`)));
 	const textareaInput = (/** @type {HTMLTextAreaElement} */ (document.querySelector(`textarea#input`)));
 	const textareaOutput = (/** @type {HTMLTextAreaElement} */ (document.querySelector(`textarea#output`)));
-	/**
-	 * 
-	 * @param {String} code 
-	 */
-	function launch(code = textareaInput.value) {
-		textareaInput.value = code;
+	function launch() {
 		if (formInterpreter.checkValidity()) {
 			const tokens = Interpreter.tokenize(textareaInput.value);
 			textareaOutput.value = tokens.map(token => token.toString()).join(`\n`);
 		}
 	}
+	//#region Search by url
 	const input = window.decodeURI(location.search).replace(/\?/, ``);
 	const search = Object.assign({}, ...input.split(`&`).map((part) => {
 		const [key, value] = part.split(`=`);
@@ -21,11 +17,14 @@ try {
 	}));
 	const expression = Reflect.get(search, `expression`);
 	if (expression) {
-		launch(expression);
+		textareaInput.value = expression;
+		launch();
 	}
+	//#endregion
 	const buttonRun = (/** @type {HTMLButtonElement} */ (document.querySelector(`button#run`)));
 	buttonRun.addEventListener(`click`, (event) => {
-		location.search = `?expression=${window.encodeURI(textareaInput.value)}`;
+		launch();
+		window.history.pushState({}, ``, `${location.origin}${location.pathname}?expression=${window.encodeURI(textareaInput.value)}`);
 	});
 	window.addEventListener(`keydown`, (event) => {
 		if (event.code == `Enter` && !event.shiftKey) {
